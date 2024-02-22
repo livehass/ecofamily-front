a70ec449f176b9ddb200ccb8508a15b6fd2812f7
Giovanni Torelli <giovanni.walter@outlook.com>
2024-02-22 11:53:32 -0300

    Created routes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 function Navbar() {
   return (
     <>
@@ -6,8 +8,34 @@ function Navbar() {
           <div className="text-2xl font-bold uppercase">ecoFamily</div>
 
           <div className="flex gap-4">
-          <div className="hover:underline">Home</div>
-          <div className="hover:underline">Sobre Nós</div>
+            <div>
+              <NavLink
+                to="/"
+                className={({ isActive, isPending }) =>
+                  isActive
+                    ? "underline"
+                    : isPending
+                    ? "animate-pulse"
+                    : "hover:underline"
+                }
+              >
+                Home
+              </NavLink>
+            </div>
+            <div>
+              <NavLink
+                to="/sobre"
+                className={({ isActive, isPending }) =>
+                  isActive
+                    ? "underline"
+                    : isPending
+                    ? "animate-pulse"
+                    : "hover:underline"
+                }
+              >
+                Sobre Nós
+              </NavLink>
+            </div>
 
             <div className="hover:underline">Produtos</div>
             <div className="hover:underline">Categorias</div>
@@ -15,7 +43,20 @@ function Navbar() {
 
             <div className="hover:underline">Perfil</div>
             <div className="hover:underline">Sair</div>
-            <div className="hover:underline">Contato</div>
+            <div className="hover:underline">
+              <NavLink
+                to="/contato"
+                className={({ isActive, isPending }) =>
+                  isActive
+                    ? "underline"
+                    : isPending
+                    ? "animate-pulse"
+                    : "hover:underline"
+                }
+              >
+                Contato
+              </NavLink>
+            </div>
           </div>
         </div>
       </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Root from './pages/root/Root.tsx'
+import Home from './pages/home/Home.tsx'
+import Contact from './pages/contact/Contact.tsx'
+import AboutUs from './pages/about/About.tsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {index: true, element: <Home /> },
+      {
+        path: "/contato",
+        element: <Contact />
+      },
+      {
+        path: "/sobre",
+        element: <AboutUs />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
