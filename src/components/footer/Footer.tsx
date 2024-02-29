3d2c29c62c7923b81124828b27cec381549ff815
carina-maleski <carinamaleski@gmail.com>
2024-02-29 09:35:17 -0300

    add date to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,11 +5,13 @@ import {
 } from "@phosphor-icons/react";
 
 function Footer() {
+  const data = new Date().getFullYear();
+
   return (
     <>
       <div className="flex justify-center bg-lime-950 text-white bg">
         <div className="container flex flex-col items-center py-4">
-          <p className="text-xl font-bold">ecoFamily | Copyright: </p>
+          <p className="text-xl font-bold">ecoFamily | Copyright: {data}</p>
           <p className="text-lg">Acesse nossas redes sociais</p>
           <div className="flex gap-2">
             <LinkedinLogo size={48} weight="bold" />
