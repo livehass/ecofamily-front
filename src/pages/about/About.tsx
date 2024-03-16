2f60350ab2812847e63270a61a8417b4b0051f16
livehass <silva.felipe12@hotmail.com>
2024-03-16 15:46:16 -0300

    Added new font style and remove background img

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,26 +1,26 @@
 const AboutUs = () => {
   return (
   
-    <section className="hero py-12 bg-fixed bg-cover bg-no-repeat bg-center relative" style={{backgroundImage: 'url("https://images.unsplash.com/photo-1516211697506-8360dbcfe9a4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90oy1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")'}}>
-  <div className="absolute inset-0 bg-white/30 backdrop-blur-lg"></div>
+    <section className="md:pt-28 flex justify-center min-h-screen hero">
+  <div className="absolute inset-0 bg-white/30 backdrop-blur-lg mt"></div>
   <div className="container mx-auto h-full">
     <div className="flex justify-end h-full relative z-10">
-      <div className="p-6 md:p-12 ">
-        <h2 className="text-4xl font-bold text-lime-950 mb-8">Sobre Nós</h2>
-        <p className="text-lg text-gray-800 leading-relaxed">
-          Bem-vindo a <span className="font-semibold text-lime-950">ecoFamily</span>, onde a paixão pelos produtos locais e a dedicação aos pequenos produtores se entrelaçam para criar uma experiência de compras única...
+      <div className="p-6 md:p-12 mt-10  ">
+        <h2 className="md:text-6xl text-4xl font-bold text-gray-900 mb-10">Sobre Nós</h2>
+        <p className="text-2xl text-gray-900 leading-relaxed">
+          Bem-vindo a <span className="font-bold text-gray-900">ecoFamily</span>, onde a paixão pelos produtos locais e a dedicação aos pequenos produtores se entrelaçam para criar uma experiência de compras única...
         </p>
   
-           <p className="text-lg text-gray-800 leading-relaxed mt-4">
-              <span className="font-semibold text-lime-950">Nossa Missão:</span> Fomentar Comunidades Locais. No coração de nossa filosofia está a missão de fortalecer as comunidades locais, valorizando a autenticidade e a singularidade de cada produto. Acreditamos que, ao apoiar pequenos produtores, contribuímos para o crescimento econômico sustentável e para a preservação das tradições locais.
+           <p className="text-2xl text-gray-900leading-relaxed mt-4">
+              <span className="font-bold text-gray-900">Nossa Missão:</span> Fomentar Comunidades Locais. No coração de nossa filosofia está a missão de fortalecer as comunidades locais, valorizando a autenticidade e a singularidade de cada produto. Acreditamos que, ao apoiar pequenos produtores, contribuímos para o crescimento econômico sustentável e para a preservação das tradições locais.
              </p>
     
-             <p className="text-lg text-gray-800 leading-relaxed mt-4">
-              <span className="font-semibold text-lime-950">O Que Nos Diferencia:</span> Qualidade, Autenticidade e Responsabilidade Social. Comprometemo-nos a oferecer produtos de alta qualidade, cuidadosamente selecionados para garantir que cada compra seja uma experiência enriquecedora. Trabalhamos lado a lado com nossos parceiros locais, colaborando para garantir que cada item reflita não apenas a habilidade artesanal, mas também a autenticidade cultural de suas origens.
+             <p className="text-2xl text-gray-900 leading-relaxed mt-4">
+              <span className="font-bold text-gray-900">O Que Nos Diferencia:</span> Qualidade, Autenticidade e Responsabilidade Social. Comprometemo-nos a oferecer produtos de alta qualidade, cuidadosamente selecionados para garantir que cada compra seja uma experiência enriquecedora. Trabalhamos lado a lado com nossos parceiros locais, colaborando para garantir que cada item reflita não apenas a habilidade artesanal, mas também a autenticidade cultural de suas origens.
             </p>
     
-             <p className="text-lg text-gray-800 leading-relaxed mt-4">
-               <span className="font-semibold text-lime-950">Junte-se a Nós nesta Jornada:</span> Apoie o Local, Valorize o Artesanal. Estamos entusiasmados em tê-lo conosco nesta jornada de descobertas e celebração dos talentos locais. Cada compra que você faz no <span className="font-semibold text-lime-950">ecoFamily</span> contribui para o fortalecimento de pequenas empresas e para a preservação das tradições locais.
+             <p className="text-2xl text-gray-900 leading-relaxed mt-4">
+               <span className="font-bold text-gray-900">Junte-se a Nós nesta Jornada:</span> Apoie o Local, Valorize o Artesanal. Estamos entusiasmados em tê-lo conosco nesta jornada de descobertas e celebração dos talentos locais. Cada compra que você faz no <span className="font-semibold text-lime-950">ecoFamily</span> contribui para o fortalecimento de pequenas empresas e para a preservação das tradições locais.
              </p>
           </div>
         </div>
